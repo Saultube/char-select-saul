dc3181d2215824ac140d744a58ab53ec6cfd3b24
Kaktus64 <134180215+Kaktus64@users.noreply.github.com>
2026-01-05 16:27:10 -0600

    palette!!

--- main.lua
+++ main.lua
@@ -12,9 +12,77 @@ function SaulUpdate(m)
     local m = network_player_from_global_index
 end
 
+local PALETTE_SAUL = {
+        [PANTS]  = "FFFFFF", --dont delete this color cuz its used in cs's paintbucket
+        [SHIRT]  = "B2CC66",
+        [GLOVES] = "FFFFFF",
+        [HAIR]   = "8E765A",
+        [SKIN]   = "FFDDA3",
+        [CAP]    = "B2CC66",
+    }
+
+    local PALETTE_CRAIG = {
+        [PANTS]  = "ffffb2", --dont delete this color cuz its used in cs's paintbucket
+        [SHIRT]  = "ce6d00",
+        [GLOVES] = "ffffb2",
+        [HAIR]   = "745200",
+        [SKIN]   = "ecbf2c",
+        [CAP]    = "ce6d00",
+    }
+
+    local PALETTE_SAUL = {
+        [PANTS]  = "FFFFFF", --dont delete this color cuz its used in cs's paintbucket
+        [SHIRT]  = "B2CC66",
+        [GLOVES] = "FFFFFF",
+        [HAIR]   = "8E765A",
+        [SKIN]   = "FFDDA3",
+        [CAP]    = "B2CC66",
+    }
+
+    local PALETTE_GOOD_SAUL = {
+        [PANTS]  = "FFFFFF", --dont delete this color cuz its used in cs's paintbucket
+        [SHIRT]  = "7d85ee",
+        [GLOVES] = "FFFFFF",
+        [HAIR]   = "8E765A",
+        [SKIN]   = "FFDDA3",
+        [CAP]    = "7d85ee",
+    }
+
+    local PALETTE_EVIL_SAUL = {
+        [PANTS]  = "FFFFFF", --dont delete this color cuz its used in cs's paintbucket
+        [SHIRT]  = "c33131",
+        [GLOVES] = "FFFFFF",
+        [HAIR]   = "8E765A",
+        [SKIN]   = "FFDDA3",
+        [CAP]    = "c33131",
+    }
+
+    local PALETTE_SAUL_JER = {
+        [PANTS]  = "4f4f4f", --dont delete this color cuz its used in cs's paintbucket
+        [SHIRT]  = "008040",
+        [GLOVES] = "4f4f4f",
+        [HAIR]   = "462C1E",
+        [SKIN]   = "ffba90",
+        [CAP]    = "00ff00",
+    }
+
+    local PALETTE_SAULOTTE = {
+        [PANTS]  = "103374", --dont delete this color cuz its used in cs's paintbucket
+        [SHIRT]  = "B814A6",
+        [GLOVES] = "103374",
+        [HAIR]   = "7B4116",
+        [SKIN]   = "FFAA6B",
+        [CAP]    = "B814A6",
+    }
+
 if _G.charSelectExists then
     CT_SAUL =_G.charSelect.character_add("Saul", "New Saul Remodel #8", "Saul, Kaktus", {r = 178, g = 204, b = 102}, E_MODEL_SAUL, CT_MARIO, TEX_SAULICON, 1.2)
-    _G.charSelect.character_add_palette_preset(E_MODEL_SAUL, PALETTE_SAUL)
+        _G.charSelect.character_add_palette_preset(E_MODEL_SAUL, PALETTE_SAUL, "Saul")
+    _G.charSelect.character_add_palette_preset(E_MODEL_SAUL, PALETTE_CRAIG, "Craig")
+    _G.charSelect.character_add_palette_preset(E_MODEL_SAUL, PALETTE_GOOD_SAUL, "Good")
+    _G.charSelect.character_add_palette_preset(E_MODEL_SAUL, PALETTE_EVIL_SAUL, "Evil")
+    _G.charSelect.character_add_palette_preset(E_MODEL_SAUL, PALETTE_SAUL_JER, "Jer")
+    _G.charSelect.character_add_palette_preset(E_MODEL_SAUL, PALETTE_SAULOTTE, "Charlotte")
     _G.charSelect.character_add_graffiti(CT_SAUL, TEX_SAULGRAF)
     _G.charSelect.character_set_category(CT_SAUL, "DXA", true)
 else
